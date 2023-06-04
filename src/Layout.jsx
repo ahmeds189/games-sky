29dcfac36c553406bceee26069838182ae879e1e
ahmed <primaryahmedsaid@gmail.com>
2023-06-04 12:01:27 +0300

    removing footer

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { AppShell, Navbar, Header, Footer, useMantineTheme } from '@mantine/core'
+import { AppShell, Navbar, Header, useMantineTheme } from '@mantine/core'
 import { NavbarContent, Games } from './components'
 
 export default function Layout({ toggleColorScheme }) {
@@ -20,11 +20,6 @@ export default function Layout({ toggleColorScheme }) {
 				<Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 250 }}>
 					Application navbar
 				</Navbar>
-			}
-			footer={
-				<Footer height={60} p="md">
-					Application footer
-				</Footer>
 			}
 			header={
 				<Header height={{ base: 60, md: 70 }}>
